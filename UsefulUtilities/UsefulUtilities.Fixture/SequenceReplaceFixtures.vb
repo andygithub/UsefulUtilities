e429db5dfacdfe6ce3edf17fde1ef0d401fee1d6
andy <andymaurer@gmail.com>
2014-10-13 13:08:46 -0400

    Sequence utility

---
+++ UsefulUtilities/UsefulUtilities.Fixture/SequenceReplaceFixtures.vb
@@ -0,0 +1,32 @@
+﻿Imports UsefulUtilities
+
+Public Class SequenceReplaceFixtures
+    <Fact>
+    Public Sub ReplaceNumberInSequence()
+        Dim seq As New List(Of Integer) From {4, 2, 42, 1337, 7}
+        Dim actual = seq.Replace(9, Function(i) i = 2).ToList
+        Assert.Equal(New List(Of Integer) From {4, 9, 42, 1337, 7}, actual)
+    End Sub
+
+    <Fact>
+    Public Sub ReplaceStringInSequence()
+        Dim seq = New List(Of String) From {"foo", "bar", "baz", "qux", "corge", "grault"}
+        Dim actual = seq.Replace("quux", "qux").ToList
+        Assert.Equal(New List(Of String) From {"foo", "bar", "baz", "quux", "corge", "grault"}, actual)
+    End Sub
+
+    <Fact>
+    Public Sub ReplaceWithNullFuncThrows()
+        Dim seq = New List(Of Object)() From {New Object(), New Object()}
+        Dim dummyValue = New Object()
+        Assert.Throws(Of ArgumentNullException)(Function() seq.Replace(dummyValue, DirectCast(Nothing, Func(Of Object, Boolean))).ToList())
+    End Sub
+
+    <Fact> _
+    Public Sub ReplaceWithNullEquatableThrows()
+        Dim seq = New List(Of Object)() From {New Object(), New Object()}
+        Dim dummyValue = New Object()
+        Assert.Throws(Of ArgumentNullException)(Function() seq.Replace(dummyValue, DirectCast(Nothing, IEquatable(Of Object))).ToList())
+    End Sub
+
+End Class
---
+++ UsefulUtilities/UsefulUtilities/Extensions/Sequence.vb
@@ -0,0 +1,17 @@
+﻿Public Module Sequence
+
+    <System.Runtime.CompilerServices.Extension>
+    Public Function Replace(Of T)(source As IEnumerable(Of T), replacementValue As T, equalityComparer As Func(Of T, Boolean)) As IEnumerable(Of T)
+        If equalityComparer Is Nothing Then Throw New ArgumentNullException("equalityComparer")
+
+        Return source.[Select](Function(x) If(equalityComparer(x), replacementValue, x))
+    End Function
+
+    <System.Runtime.CompilerServices.Extension>
+    Public Function Replace(Of T)(source As IEnumerable(Of T), replacementValue As T, equalityComparer As IEquatable(Of T)) As IEnumerable(Of T)
+        If equalityComparer Is Nothing Then Throw New ArgumentNullException("equalityComparer")
+
+        Return source.[Select](Function(x) If(equalityComparer.Equals(x), replacementValue, x))
+    End Function
+
+End Module
